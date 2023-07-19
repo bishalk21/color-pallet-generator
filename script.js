72bb756f8d76b45645e32fd233ce3eb61140e509
Your Name <karkibishal00@gmail.com>
2023-07-19 18:16:17 +1000

    day20: made a simple color pallet generator

---
+++ script.js
@@ -0,0 +1,29 @@
+const pallet = document.getElementById("pallet");
+const generateBtn = document.getElementById("generate");
+
+function generateRandomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  return `rgb(${red},${green},${blue})`;
+}
+
+function generate() {
+  pallet.innerHTML = "";
+  for (let i = 0; i < 5; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = generateRandomColor();
+    div.classList.add("col");
+
+    let copy = document.createElement("div");
+    copy.innerHTML = `<p>${div.style.backgroundColor}</p>`;
+    copy.classList.add("copy");
+
+    div.appendChild(copy);
+    pallet.appendChild(div);
+  }
+}
+
+generate();
+
+generateBtn.addEventListener("click", generate());
